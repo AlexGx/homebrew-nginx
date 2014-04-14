548b8f86dacd6e1b35736e98f7972207673b3dce
AlexGx <gubarev.alex@gmail.com>
2014-04-14 08:22:26 +0300

    replace url source

--- push-stream-nginx-module.rb
+++ push-stream-nginx-module.rb
@@ -2,9 +2,7 @@ require 'formula'
 
 class PushStreamNginxModule < Formula
   homepage 'https://github.com/wandenberg/nginx-push-stream-module'
-  url 'https://github.com/wandenberg/nginx-push-stream-module/archive/0.4.0.tar.gz'
-  sha1 '1ec3691c5ee0d3d6ef058f61e9f15c6decf55448'
-  version '0.4.0'
+  url 'git://github.com/wandenberg/nginx-push-stream-module.git', :branch => "0.4.1-dev", :using => :git
 
   def install
     (share+'push-stream-nginx-module').install Dir['*']
